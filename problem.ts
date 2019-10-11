35e3a6ad0204e4143e569c5c0216251d58d2c671
Turkington, Kevin <kevin.turkington@nordstrom.com>
2019-10-11 16:14:32 -0700

    adding singleton problem

---
+++ problem.ts
@@ -0,0 +1,14 @@
+/**
+ * Problem 1 - number counter
+ * Design a singleton that will return a number each time its called,
+ * and each time it iterates it increments that number (0,1,2,3,..., n)
+ * 
+ * Follow up: 
+ * Edit your singleton so that it alternates between returning a your incrementing number
+ * and a letter from the alphabet
+ * 
+ * Example output:
+ * 0, A, 1, B, 2, C,....
+ */
+
+ console.log("Hello world!")
